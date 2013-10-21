cad94096e370528ebe875df4addfaa0f5e9cf124
Caleb Spare <cespare@gmail.com>
2013-10-21 15:42:42 -0700

    Add criterium to lein profiles

--- .lein/profiles.clj
+++ .lein/profiles.clj
@@ -1,2 +1,3 @@
 {:user {:plugins [[lein-exec "0.3.0"]]
-        :dependencies [[org.clojure/tools.namespace "0.2.4"]]}}
+        :dependencies [[org.clojure/tools.namespace "0.2.4"]
+                       [criterium "0.4.2"]]}}
