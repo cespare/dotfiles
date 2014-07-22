bdcdfc7f612addfe4aaa042b96aaffc029810083
Caleb Spare <cespare@gmail.com>
2014-07-21 17:35:49 -0700

    Lein profiles changes

--- .lein/profiles.clj
+++ .lein/profiles.clj
@@ -1,4 +1,6 @@
 {:user {:plugins [[lein-exec "0.3.0"]
                   [jonase/eastwood "0.1.2"]]
         :dependencies [[org.clojure/tools.namespace "0.2.4"]
-                       [criterium "0.4.2"]]}}
+                       [criterium "0.4.2"]
+                       [slamhound "1.5.5"]]
+        :aliases {"slamhound" ["run" "-m" "slam.hound"]}}}
