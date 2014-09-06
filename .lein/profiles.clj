323476068ce2bedc1638da9faf2d0dce93e3467b
Caleb Spare <cespare@gmail.com>
2014-09-05 20:52:35 -0700

    lein-depgraph is handy

--- .lein/profiles.clj
+++ .lein/profiles.clj
@@ -1,4 +1,5 @@
 {:user {:plugins [[lein-exec "0.3.0"]
+                  [lein-depgraph "0.1.0"]
                   [jonase/eastwood "0.1.2"]]
         :dependencies [[org.clojure/tools.namespace "0.2.4"]
                        [criterium "0.4.2"]
