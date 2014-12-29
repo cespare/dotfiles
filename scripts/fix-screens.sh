491ed1971ab68a4f5cfe6b12ddeea7372e367b2c
Caleb Spare <cespare@gmail.com>
2014-12-29 11:49:19 -0800

    Update screen setup script

--- scripts/fix-screens.sh
+++ scripts/fix-screens.sh
@@ -2,8 +2,5 @@
 
 # Fix monitors
 
-# 2 monitors
-#xrandr --output DisplayPort-0 --mode 2560x1600 --pos 0x320 --rotate normal --output DVI-0 --off --output DVI-1 --mode 1920x1200 --pos 2560x0 --rotate left --output HDMI-0 --off
-
 # 3 monitors
-xrandr --output DisplayPort-0 --mode 2560x1600 --pos 1200x480 --rotate normal --output DVI-0 --mode 1920x1200 --pos 0x320 --rotate left  --output DVI-1 --mode 2560x1600 --pos 3760x0 --rotate left --output HDMI-0 --off
+xrandr --output DVI-D-0 --mode 2560x1600 --pos 0x0 --rotate left --output DP-1 --mode 2560x1600 --pos 1600x480 --rotate normal --output DVI-I-1 --mode 1920x1200 --pos 4160x320 --rotate left
