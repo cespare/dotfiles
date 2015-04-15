c1dad674761186aa8137365f0e585784cae859e7
Caleb Spare <cespare@gmail.com>
2015-04-14 18:28:34 -0700

    Update fix-screens

--- scripts/fix-screens.sh
+++ scripts/fix-screens.sh
@@ -3,4 +3,4 @@
 # Fix monitors
 
 # 3 monitors
-xrandr --output DVI-D-0 --mode 2560x1600 --pos 0x0 --rotate left --output DP-1 --mode 2560x1600 --pos 1600x480 --rotate normal --output DVI-I-1 --mode 1920x1200 --pos 4160x320 --rotate left
+xrandr --output DVI-D-1 --mode 2560x1600 --pos 0x0 --rotate left --output DP-1 --mode 2560x1600 --pos 1600x480 --rotate normal --output DVI-I-1 --mode 1920x1200 --pos 4160x320 --rotate left
