c7b91c233e21ef19113ee7dacf9d22ac3c3efe45
Caleb Spare <cespare@gmail.com>
2016-01-15 17:03:35 -0800

    [.lein] Remove eastwood; it's breaking 'lein new'

--- .lein/profiles.clj
+++ .lein/profiles.clj
@@ -1,7 +1,6 @@
 {:user {:plugins [[lein-exec "0.3.0"]
                   [lein-depgraph "0.1.0"]
-                  [lein-deps-tree "0.1.2"]
-                  [jonase/eastwood "0.1.2"]]
+                  [lein-deps-tree "0.1.2"]]
         :dependencies [[org.clojure/tools.namespace "0.2.4"]
                        [criterium "0.4.2"]
                        [slamhound "1.5.5"]]
